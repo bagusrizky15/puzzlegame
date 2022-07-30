9d1b4fcdcef011f3d5564a132dd988d1cbf1855f
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-07-30 22:15:26 +0700

    add function

--- app/src/main/java/com/rivvana/puzzle_game/MainActivity.java
+++ app/src/main/java/com/rivvana/puzzle_game/MainActivity.java
@@ -2,13 +2,26 @@ package com.rivvana.puzzle_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonMulai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMulai = findViewById(R.id.btnMulai);
+        buttonMulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
